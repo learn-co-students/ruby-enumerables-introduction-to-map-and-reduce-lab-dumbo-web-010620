aa2dd35d02eb89fb625269c3f6b4eaa738d9c90c
Julianna Boyd <julianna.boyd91@gmail.com>
2019-12-22 00:00:41 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -10,7 +10,13 @@ return array
 end 
 
 def map_to_no_change(source_array)
-  source_array
+  array = []
+  i = 0
+while i < source_array.length do 
+  array.push(source_array[i])
+  i += 1 
+end 
+return array 
 end 
 
 def map_to_double(source_array)
@@ -27,7 +33,7 @@ def map_to_square(source_array)
   array = [] 
   i = 0 
 while i < source_array.length do 
-  array.push(source_array[i] **2)
+  array.push(source_array[i] * source_array[i])
   i += i 
 end 
 return array
